bc90c90e968b34ed680ab631309b1971a47b050c
czFIRE <p1kadlec@gmail.com>
2019-11-24 21:06:29 +0100

    Made few small changes and increased epochs for more credibility.

--- src/JavaNN/Main.java
+++ src/JavaNN/Main.java
@@ -3,22 +3,30 @@ package JavaNN;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("I work!");
-        System.out.println("Seed: " + Utils.getSeed());
+        //System.out.println("I work!");
+        //System.out.println("Seed: " + Utils.getSeed());
         //new Test();
 
-        Utils.setSeed(138);   //change for better results
-        System.out.println("Seed: " + Utils.getSeed());
+        //Utils.setSeed(138);   //change for better results 138
+        //System.out.println("Seed: " + Utils.getSeed());
 
-        int epochs = 16;
+        int epochs = 30;    //16
         double learningRate = 0.05;
         double momentum = 0.80;
-        int []architecture = new int[] {784, 128, 10};
-        int miniBatchSize = 200;
+        int []architecture = new int[] {784, 128, 10};  //728,128,10
+        int miniBatchSize = 200;            //200
+
+        System.out.println("Seed: " + Utils.getSeed());
+        System.out.println("Epochs: " + epochs);
+        System.out.println("Learning rate: " + learningRate);
+        System.out.println("Momentum: " + momentum);
+        System.out.println("Mini batch size: " + miniBatchSize);
+        System.out.println("Architecture: " + Arrays.toString(architecture));
         /* Data provided to us by the teacher */
         //D:\Java\JavaNeuralNetwork\MNIST_DATA\
         String trainData = "D:\\Java\\JavaNeuralNetwork\\MNIST_DATA\\mnist_train_vectors.csv";
@@ -53,7 +61,7 @@ public class Main {
             }
             mlp.setLearningRate(learningRate/Math.sqrt(epoch + 1));
         }
-        System.out.println("done learning");
+        System.out.println("Done learning, starting evaluation");
 
         double [][][] testLabelsCheck = new double [Utils.dataSetLength/miniBatchSize][][];
         for (int i = 0; i < Utils.dataSetLength/miniBatchSize; i++) {
@@ -64,6 +72,7 @@ public class Main {
         DataReader.write("D:\\Java\\JavaNeuralNetwork\\evaluator\\trainPredictions", testLabelsCheck);
         //D:\Java\JavaNeuralNetwork\evaluator\trainPredictions
 
+        System.out.println("Finished evaluating train set.");
         try {
             control = new BufferedReader(new FileReader(testData));
         } catch (IOException e) {
@@ -82,5 +91,6 @@ public class Main {
         }
 
         DataReader.write("D:\\Java\\JavaNeuralNetwork\\evaluator\\actualTestPredictions", answers);
+        System.out.println("Finished evaluating test set.");
     }
 }
